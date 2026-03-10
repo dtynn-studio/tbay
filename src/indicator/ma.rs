3215a7e7e861a704d4d4f02cc1c284864ae6da22
dtynn <dtynn@163.com>
2026-03-10 12:14:38 +0800

    feat(indicator): impl ::ma::Ma

--- src/indicator/ma.rs
+++ src/indicator/ma.rs
@@ -3,3 +3,37 @@ mod sma;
 
 pub use ema::Ema;
 pub use sma::Sma;
+
+use crate::prelude::{Decimal, Indicator};
+
+pub enum Ma {
+    Sma(Sma),
+    Ema(Ema),
+}
+
+impl Indicator for Ma {
+    type State = Decimal;
+    type Item<'a> = Decimal;
+    type Value = Decimal;
+
+    fn state(&self) -> Option<&Self::State> {
+        match self {
+            Self::Sma(m) => m.state(),
+            Self::Ema(m) => m.state(),
+        }
+    }
+
+    fn calc<'a>(&self, next: Self::Item<'a>) -> Option<Self::Value> {
+        match self {
+            Self::Sma(m) => m.calc(next),
+            Self::Ema(m) => m.calc(next),
+        }
+    }
+
+    fn update<'a>(&mut self, next: Self::Item<'a>) -> Option<Self::Value> {
+        match self {
+            Self::Sma(m) => m.update(next),
+            Self::Ema(m) => m.update(next),
+        }
+    }
+}
