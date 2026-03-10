61fa2933ca54fcec5919f68f317052da05efc814
dtynn <dtynn@163.com>
2026-03-10 13:10:19 +0800

    feat(indicator): impl PriceMa

--- src/indicator/base/price.rs
+++ src/indicator/base/price.rs
@@ -1,8 +1,13 @@
-use std::str::FromStr;
+use std::{borrow::Cow, str::FromStr};
+
+use scanf::sscanf;
+use snafu::ResultExt;
 
 use crate::{
-    indicator::ma::Ma,
-    prelude::{BaseIndicator, Decimal, Error, Indicator, KInfo},
+    indicator::ma::{Ema, Ma, Sma},
+    prelude::{
+        BaseIndicator, Decimal, Error, Indicator, KInfo, ParseCtx, Unexpected,
+    },
 };
 
 fn close_extractor(info: &KInfo) -> Decimal {
@@ -10,6 +15,7 @@ fn close_extractor(info: &KInfo) -> Decimal {
 }
 
 pub struct PriceMa {
+    key: String,
     ma: Ma,
     extractor: fn(&KInfo) -> Decimal,
 }
@@ -17,7 +23,37 @@ pub struct PriceMa {
 impl FromStr for PriceMa {
     type Err = Error;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        unimplemented!()
+        let mut period: usize = 0;
+        let mut price_kind: String = String::new();
+        let mut ma_kind: String = String::new();
+
+        sscanf!(s, "{price_kind}:{ma_kind}:{period}").with_context(|_| {
+            ParseCtx {
+                raw: s.to_owned(),
+                usage: Cow::from("parse PriceMa"),
+            }
+        })?;
+
+        let extractor = match price_kind.as_str() {
+            "close" => close_extractor,
+            other => return Err(other.unexpected("price kind")),
+        };
+
+        if period == 0 {
+            return Err(period.unexpected("ma period"));
+        }
+
+        let ma = match ma_kind.as_str() {
+            "ema" => Ma::Ema(Ema::new(period)),
+            "sma" => Ma::Sma(Sma::new(period)),
+            other => return Err(other.unexpected("ma kind")),
+        };
+
+        Ok(Self {
+            key: s.to_owned(),
+            ma,
+            extractor,
+        })
     }
 }
 
@@ -27,20 +63,22 @@ impl Indicator for PriceMa {
     type Value = Decimal;
 
     fn state(&self) -> Option<&Self::State> {
-        unimplemented!()
+        self.ma.state()
     }
 
-    fn calc<'a>(&self, ext: Self::Item<'a>) -> Option<Self::Value> {
-        unimplemented!()
+    fn calc<'a>(&self, next: Self::Item<'a>) -> Option<Self::Value> {
+        let next = (self.extractor)(next);
+        self.ma.calc(next)
     }
 
     fn update<'a>(&mut self, next: Self::Item<'a>) -> Option<Self::Value> {
-        unimplemented!()
+        let next = (self.extractor)(next);
+        self.ma.update(next)
     }
 }
 
 impl<'a> BaseIndicator<'a> for PriceMa {
     fn key(&self) -> &str {
-        unimplemented!()
+        &self.key
     }
 }
--- src/res.rs
+++ src/res.rs
@@ -1,3 +1,42 @@
+use std::{borrow::Cow, fmt::Display};
+
+use snafu::Snafu;
+
 pub type Result<T, E = Error> = std::result::Result<T, E>;
 
-pub enum Error {}
+#[derive(Debug, Snafu)]
+#[snafu(context(suffix(Ctx)))]
+#[snafu(visibility(pub))]
+pub enum Error {
+    #[snafu(display("parse `{raw}` for {usage}"))]
+    Parse {
+        raw: String,
+        usage: Cow<'static, str>,
+        source: std::io::Error,
+    },
+
+    #[snafu(display("{reason}"))]
+    Msg { reason: Cow<'static, str> },
+}
+
+pub trait Unexpected<T> {
+    fn unexpected(self, msg: &str) -> Error;
+}
+
+impl<T: Display> Unexpected<T> for T {
+    fn unexpected(self, usage: &str) -> Error {
+        Error::Msg {
+            reason: format!(
+                "unexpected value {self} of type {}: {usage}",
+                std::any::type_name::<T>()
+            )
+            .into(),
+        }
+    }
+}
+
+pub mod prelude {
+    pub use snafu::prelude::*;
+
+    pub use super::*;
+}
