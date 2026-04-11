ca65c2b785c48ba8de6d7a453cf1d0d085d7b176
dtynn <dtynn@163.com>
2026-04-11 11:56:18 +0800

    refact(k): KInfo::trend

--- src/k.rs
+++ src/k.rs
@@ -104,7 +104,6 @@ pub struct KInfo {
     pub body: PriceBar,
     pub full: PriceBar,
     pub shadow: KShadow,
-    pub trend: Trend,
 }
 
 impl From<KRaw> for KInfo {
@@ -136,46 +135,13 @@ impl From<KRaw> for KInfo {
             below: body.low - full.low,
         };
 
-        let trend = kinfo_trend(body, full, shadow, direction, None);
-
         Self {
             raw: value,
             direction,
             body,
             full,
             shadow,
-            trend,
         }
-    }
-}
-
-fn kinfo_trend(
-    body: PriceBar,
-    full: PriceBar,
-    shadow: KShadow,
-    direction: Option<bool>,
-    threshold: Option<Decimal>,
-) -> Trend {
-    let threshold = threshold.unwrap_or(Decimal::from(2) / Decimal::from(3));
-
-    if full.height.is_zero() {
-        return Trend::Unknown;
-    }
-
-    let body_height = body.high - body.low;
-
-    if body_height / full.height >= threshold {
-        match direction {
-            Some(true) => Trend::Up,
-            Some(false) => Trend::Down,
-            None => Trend::Unknown,
-        }
-    } else if shadow.above / full.height >= threshold {
-        Trend::Down
-    } else if shadow.below / full.height >= threshold {
-        Trend::Up
-    } else {
-        Trend::Unknown
     }
 }
 
@@ -191,6 +157,53 @@ impl KInfo {
 
     pub fn is_not_below(&self, base: Decimal) -> bool {
         !matches!(self.relative_position(base), RelativePosition::Below)
+    }
+
+    pub fn trend(
+        &self,
+        single_threshold: Decimal,
+        mixed_threshold: Decimal,
+    ) -> Trend {
+        if self.full.height.is_zero() {
+            return Trend::Unknown;
+        }
+
+        let body_ratio = self.body.height / self.full.height;
+        let above_ratio = self.shadow.above / self.full.height;
+        let below_ratio = self.shadow.below / self.full.height;
+
+        if body_ratio >= single_threshold {
+            match self.direction {
+                Some(true) => Trend::Up,
+                Some(false) => Trend::Down,
+                None => Trend::Unknown,
+            }
+        } else if above_ratio >= single_threshold {
+            Trend::Down
+        } else if below_ratio >= single_threshold {
+            Trend::Up
+        } else {
+            // 没有单个部分满足占比
+            match self.direction {
+                Some(true) => {
+                    if (below_ratio + body_ratio) >= mixed_threshold {
+                        Trend::Up
+                    } else {
+                        Trend::Unknown
+                    }
+                }
+
+                Some(false) => {
+                    if (above_ratio + body_ratio) >= mixed_threshold {
+                        Trend::Down
+                    } else {
+                        Trend::Unknown
+                    }
+                }
+
+                None => Trend::Unknown,
+            }
+        }
     }
 }
 
