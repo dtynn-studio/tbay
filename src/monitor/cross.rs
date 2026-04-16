1abf9c442215ff5df186da2cf54edf60b8e91024
dtynn <dtynn@163.com>
2026-04-16 08:08:04 +0800

    feat(monitor::cross): send alerts only for finalized ks

--- src/monitor/cross.rs
+++ src/monitor/cross.rs
@@ -45,7 +45,6 @@ impl Args for CrossArgs {
             current: None,
             state: Default::default(),
             alerts: Default::default(),
-            temp_t: None,
         })
     }
 }
@@ -60,7 +59,6 @@ pub struct Cross {
     current: Option<CrossValue<Decimal>>,
     state: State,
     alerts: AlertManager,
-    temp_t: Option<OffsetDateTime>,
 }
 
 impl Cross {
@@ -125,16 +123,12 @@ impl Monitor for Cross {
             self.state.temp.take();
             self.state.perm =
                 self.update(kctx).map(|msg| (kctx.info.raw.time_begin, msg));
-        } else {
-            let prev_temp_t = self.temp_t.replace(kctx.info.raw.time_begin);
-            if prev_temp_t != self.temp_t || self.state.temp.is_none() {
-                self.state.temp =
-                    self.calc(kctx).map(|msg| (kctx.info.raw.time_begin, msg));
-
-                if let Some((t, msg)) = self.state.temp.as_ref().cloned() {
-                    self.alerts.add(t, msg);
-                }
+            if let Some((t, msg)) = self.state.perm.as_ref().cloned() {
+                self.alerts.add(t, msg);
             }
+        } else {
+            self.state.temp =
+                self.calc(kctx).map(|msg| (kctx.info.raw.time_begin, msg));
         }
     }
 
