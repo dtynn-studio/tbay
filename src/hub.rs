fb065eb5ab28c53a94da3dd781502554816b2243
dtynn <dtynn@163.com>
2026-04-23 23:13:37 +0800

    feat(hub): register monitor::burst::BurstBuilder

--- src/hub.rs
+++ src/hub.rs
@@ -84,6 +84,7 @@ impl Default for Hub {
         hub.register_monitor_builder(monitor::fbq::FBQBuilder);
         hub.register_monitor_builder(monitor::reach::ReachBuilder);
         hub.register_monitor_builder(monitor::cross2::Cross2Builer);
+        hub.register_monitor_builder(monitor::burst::BurstBuilder);
 
         hub
     }
