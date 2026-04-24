132139cdedcda3a58be1e101a241b61b09c8692b
dtynn <dtynn@163.com>
2026-04-24 14:12:10 +0800

    feat(monitor): impl TempAlertChecker

--- src/monitor/alert.rs
+++ src/monitor/alert.rs
@@ -1,6 +1,8 @@
+use std::time::Duration;
+
 use time::OffsetDateTime;
 
-use crate::monitor::Msg;
+use crate::prelude::*;
 
 #[derive(Default)]
 pub struct AlertManager {
@@ -18,5 +20,34 @@ impl AlertManager {
         } else {
             vec![]
         }
+    }
+}
+
+const TEMP_ALERT_GAP: Duration = Duration::from_secs(2);
+
+#[derive(Default)]
+pub struct TempAlertChecker {
+    prev_t: Option<OffsetDateTime>,
+}
+
+impl TempAlertChecker {
+    pub fn allow(&mut self, kctx: &KCtx) -> bool {
+        let t = kctx.info.t();
+        if kctx.info.raw.finalized || self.prev_t == Some(t) {
+            return false;
+        }
+
+        let Ok(now) = OffsetDateTime::now_local() else {
+            return false;
+        };
+
+        let gap = kctx.info.raw.time_end - now;
+        if gap > TEMP_ALERT_GAP {
+            return false;
+        }
+
+        self.prev_t.replace(t);
+
+        true
     }
 }
